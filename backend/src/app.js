271761abcd7a33e9efab3428fe05d03d84664c7d
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-05 22:22:03 +0100

    Connected category routes to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ require('./database/db');
 const express = require('express');
 const app = express(); // creates the backend application object
 const restaurantRoutes = require('./routes/restaurantRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
 
 app.use(express.json());
 
@@ -10,7 +11,8 @@ app.get('/health', (req, res) => {
     res.json({status: 'OK'});
 })
 
-app.use('/restaurants', restaurantRoutes); // send any request starting with /restaurants to restaurantRoutes
+app.use('/restaurants', restaurantRoutes); // send any request starting with /restaurants (prefix) to restaurantRoutes
+app.use('/restaurants', categoryRoutes);
 
 module.exports = app; // allows other files to use this backend definition
 
--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 const { getCategoriesByRestaurant } = require('../controllers/categoryController');
 
 // GET /restaurants/:restaurantId/categories
-router.get('/restaurants/:restaurant_id/categories', getCategoriesByRestaurant); // when a get request matches the URL pattern, call the func
+router.get('/:restaurant_id/categories', getCategoriesByRestaurant); // when a get request matches the URL pattern, call the func
 
 module.exports = router;
