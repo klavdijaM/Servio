f8ed90f404b77a21e6f0c47aa97ab18b3c0f6a9a
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-06 23:57:47 +0100

    implemented order and order-items  creation in controller

---
+++ backend/src/controllers/orderController.js
@@ -0,0 +1,66 @@
+const db = require('../database/db');
+
+// POST /orders => inserting orders into the database
+function createOrder(req, res) {
+    const { userId, restaurantId, items, voucherId } = req.body;
+
+    if (!userId || !restaurantId || !Array.isArray(items) || items.length === 0) {
+        return res.status(400).json({ error: 'Invalid order data' });
+    }
+
+    db.serialize(() => {
+        db.run('BEGIN TRANSACTION');
+
+        const insertOrderQuery = `
+        INSERT INTO orders (user_id, restaurant_id, status, voucher_id)
+        VALUES (?, ?, ?, ?)`;
+
+        db.run(
+            insertOrderQuery,
+            [userId, restaurantId, 'created', voucherId ?? null],
+            function (err) {
+                if (err) {
+                    db.run('ROLLBACK');
+                    return res.status(500).json({ error: 'Failed to create order' });
+                }
+                const orderId = this.lastID; // the order we created (id is auto-generated)
+
+                const insertItemQuery = `
+                INSERT INTO order_items (order_id, dish_id, quantity, price)
+                VALUES (?, ?, ?, ?)`;
+
+                let completed = 0;
+                let failed = false;
+
+                items.forEach(item => {
+                    db.run(
+                        insertItemQuery,
+                        [orderId, item.dishId, item.quantity, item.price ],
+                        (err) => {
+                            if (err && !failed)  {
+                                failed = true;
+                                db.run('ROLLBACK');
+                                return res.status(500).json({ error: 'Failed to create order items' });
+                            }
+
+                            completed++; // counts successful increments
+
+                            if (completed === items.length && !failed) {
+                                db.run('COMMIT', (commitErr) => {
+                                    if (commitErr) {
+                                        db.run('ROLLBACK');
+                                        return res.status(500).json({ error: 'Failed to finalize order' });
+                                    }
+
+                                    res.status(201).json({ message: 'Order created' , orderId });
+                                });
+                            }
+
+                        }
+                    );
+                });
+
+            }
+        )
+    })
+}
--- backend/src/database/seed.js
+++ backend/src/database/seed.js
@@ -1004,7 +1004,7 @@ db.serialize(() => { // executes SQL commands in order, no skipping ahead while 
     });
     console.log('Vouchers seeded');
 
-})
+});
 
 
 
