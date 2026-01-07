e0ca94fecb581ae2bf1c2137e34b5a0a37accc83
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-07 18:56:41 +0100

    Added GET /orders route handler

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -3,12 +3,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { createOrder, getOrderById } = require('../controllers/orderController'); // destructuring of the object: pulls out the two functions
+const { createOrder, getOrderById, getOrdersByUser} = require('../controllers/orderController'); // destructuring of the object: pulls out the two functions
 
 // POST /orders
 router.post('/', createOrder);
 
 // GET /orders/:id
 router.get('/:id', getOrderById);
+
+// GET /orders
+router.get('/', getOrdersByUser);
 
 module.exports = router;
