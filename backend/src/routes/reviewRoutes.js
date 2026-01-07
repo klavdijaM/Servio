c64c40841d82b096a419e881e87f6e783105edb7
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-07 21:39:06 +0100

    Added routes for creating and fetching reviews

---
+++ backend/src/routes/reviewRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const { createReview, getReviewsByRestaurant } = require('../controllers/reviewController');
+
+// POST /reviews
+router.post('/', createReview);
+
+// GET /restaurants/:id/reviews
+router.get('/restaurants/:id/reviews', getReviewsByRestaurant);
+
+module.exports = router;
