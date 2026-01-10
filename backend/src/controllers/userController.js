c341363015a6d492ccf96901ceab71174fa76edd
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-10 15:34:24 +0100

    Implemented controller logic for updating user password

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,5 @@
 const db = require('../database/db');
+const bcrypt = require('bcrypt');
 
 // GET /users/me
 function getCurrentUser(req, res) {
@@ -27,6 +28,53 @@ function getCurrentUser(req, res) {
     });
 }
 
+// PUT /users/password
+function updatePassword(req, res) {
+    const { userId, currentPassword, newPassword } = req.body;
+
+    if (!userId || !currentPassword || !newPassword) {
+        return res.status(400).json({ error: 'Missing required data' });
+    }
+
+    const query = `
+        SELECT id, password
+        FROM users
+        WHERE id = ?
+    `;
+
+    db.get(query, [userId], (err, user) => {
+        if (err) {
+            return res.status(500).json({ error: 'Database error' });
+        }
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const passwordMatches = bcrypt.compareSync(currentPassword, user.password);
+
+        if (!passwordMatches) {
+            return res.status(401).json({ error: 'Current password is not correct' });
+        }
+
+        const newHashedPassword = bcrypt.hashSync(newPassword, 10);
+
+        const updateQuery = `
+        UPDATE users
+        SET password = ?
+        WHERE id = ?`
+
+        db.run(updateQuery, [newHashedPassword, userId], (err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Failed to update password' });
+            }
+
+            res.json({ message: 'Password updated successfully' });
+        })
+
+    })
+}
+
 module.exports = {
-    getCurrentUser
+    getCurrentUser,
+    updatePassword
 };
