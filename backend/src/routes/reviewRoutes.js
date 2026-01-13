9b8736611b40aa05f7846724461773117d9eb450
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-13 23:09:23 +0100

    Added authentication middleware to reviewRoutes

--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const authMiddleware = require('../middleware/authMiddleware');
 const { createReview, getReviewsByRestaurant } = require('../controllers/reviewController');
 
 // POST /reviews
-router.post('/', createReview);
+router.post('/', authMiddleware, createReview);
 
 module.exports = router;
