8a8097e161e9298dba435be0e82db22a4e70363c
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-13 23:05:18 +0100

    Added authentication middleware to userRoutes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const authMiddleware = require('../middleware/authMiddleware');
 const { getCurrentUser, updatePassword} = require('../controllers/userController');
 
 // GET /users/me
-router.get('/me', getCurrentUser);
+router.get('/me', authMiddleware, getCurrentUser);
 
 // PUT /users/password
-router.put('/password', updatePassword);
+router.put('/password', authMiddleware, updatePassword);
 
 module.exports = router;
