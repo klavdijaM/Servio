81c959e25bbafd49824fea681b7bcf332e07e638
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-13 22:53:54 +0100

    Added authentication middleware to order routes

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -3,15 +3,16 @@
 const express = require('express');
 const router = express.Router();
 
+const authMiddleware = require('../middleware/authMiddleware');
 const { createOrder, getOrderById, getOrdersByUser} = require('../controllers/orderController'); // destructuring of the object: pulls out the two functions
 
 // POST /orders
-router.post('/', createOrder);
+router.post('/', authMiddleware, createOrder);
 
 // GET /orders/:id
-router.get('/:id', getOrderById);
+router.get('/:id', authMiddleware, getOrderById);
 
 // GET /orders
-router.get('/', getOrdersByUser);
+router.get('/', authMiddleware, getOrdersByUser);
 
 module.exports = router;
