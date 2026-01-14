4014abbd9d6393221291b4439039228ce4b164ac
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-14 21:39:13 +0100

    Added hero image aset and simple html landing page structure

--- frontend/servio-frontend/src/app/app.ts
+++ frontend/servio-frontend/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+// root UI component definition
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: 'app-root', // this component will be rendered when app-root appears in html
+  imports: [RouterOutlet], // the component is allowed to use router-outlet in its html
+  templateUrl: './app.html', // defines the location of the html for this component
+  styleUrl: './app.css' // defines the location of css
 })
 export class App {
-  protected readonly title = signal('servio-frontend');
+  protected readonly title = signal('servio-frontend'); // signal = variable watched by Angular in case of changes
 }
