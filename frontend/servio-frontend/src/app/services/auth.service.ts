1b1604a8f60d931301553b333880ac1c2d68ab1c
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-24 20:34:59 +0100

    Updated AuthService to store JWT token, check login state, and handle logout

--- frontend/servio-frontend/src/app/services/auth.service.ts
+++ frontend/servio-frontend/src/app/services/auth.service.ts
@@ -4,10 +4,11 @@ import { HttpClient } from '@angular/common/http'; // angular HTTP API - allows 
 // service handles logic which is not UI related (data, backend communication)
 // sends login and register data to the backend
 @Injectable({
-  providedIn: 'root' // singleton - one instance of the service for entire app
+  providedIn: 'root'// singleton - one instance of the service for entire app
 })
 export class AuthService {
   private apiUrl = 'http://localhost:3000/auth'; // defines a base path
+  private TOKEN_KEY = 'auth_token';
 
   constructor(private http: HttpClient) {} // HttpClient builds http requests
 
@@ -21,9 +22,25 @@ export class AuthService {
   }
 
   login(email: string, password: string) {
-    return this.http.post<any>(`${this.apiUrl}/login`, {
+    return this.http.post<{token: string}>(`${this.apiUrl}/login`, { // request will be sent when someone subscribes
       email,
       password
     });
+  }
+
+  storeToken(token: string) {
+    localStorage.setItem('TOKEN_KEY', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  isLoggedIn(): boolean{
+    return !!this.getToken();
+  }
+
+  logout() {
+    localStorage.removeItem(this.TOKEN_KEY);
   }
 }
