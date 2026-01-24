368695ae2029426ff58a6b43d32391412b4d8e7b
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-24 17:27:54 +0100

    Implemented login authentication logic

--- frontend/servio-frontend/src/app/components/login-modal/login-modal.ts
+++ frontend/servio-frontend/src/app/components/login-modal/login-modal.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-modal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-modal.html',
   styleUrl: './login-modal.css',
 })
 export class LoginModal {
+  email: string = '';
+  password: string = '';
+
+  @Output() closed = new EventEmitter<void>();
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => {
+        // add token authentication logic
+        this.close();
+      },
+      error: (err) => {
+        alert(err.error?.error || 'Login failed.');
+      }
+    })
+  }
+
+  close(){
+    this.closed.emit();
+  }
+
+
 
 }
