8bab6016ecf08c318dd4eec5df0e119b9b4a20bd
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-24 17:54:51 +0100

    Connected login button to login modal

--- frontend/servio-frontend/src/app/app.ts
+++ frontend/servio-frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterModal } from './components/register-modal/register-modal';
+import {LoginModal} from './components/login-modal/login-modal';
 
 // root UI component definition
 @Component({
   selector: 'app-root', // this component will be rendered when app-root appears in html
-  imports: [RegisterModal], // what the component is allowed to use
+  imports: [RegisterModal, LoginModal ], // what the component is allowed to use
   templateUrl: './app.html', // defines the location of the html for this component
   styleUrl: './app.css' // defines the location of css
 })
@@ -14,6 +15,7 @@ import { RegisterModal } from './components/register-modal/register-modal';
 export class App {
 
   showRegisterModal = false;
+  showLoginModal = false;
 
   openRegister() {
     this.showRegisterModal = true;
@@ -22,4 +24,13 @@ export class App {
   closeRegister() {
     this.showRegisterModal = false;
   }
+
+  openLogin() {
+    this.showLoginModal = true;
+  }
+
+  closeLogin() {
+    this.showLoginModal = false;
+  }
+
 }
