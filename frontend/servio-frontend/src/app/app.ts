6d0c66be45529fa3106e1cef671183dc23f69325
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-26 19:56:55 +0100

    Wired registration success flow and modal transitions

--- frontend/servio-frontend/src/app/app.ts
+++ frontend/servio-frontend/src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, signal } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { RouterOutlet } from '@angular/router';
 import { RegisterModal } from './components/register-modal/register-modal';
+import { RegistrationSuccessModal } from './components/registration-success-modal/registration-success-modal';
 import {LoginModal} from './components/login-modal/login-modal';
 
 // root UI component definition
 @Component({
   selector: 'app-root', // this component will be rendered when app-root appears in html
-  imports: [RegisterModal, LoginModal ], // what the component is allowed to use
+  imports: [RegisterModal, LoginModal, RegistrationSuccessModal ], // what the component is allowed to use
   templateUrl: './app.html', // defines the location of the html for this component
   styleUrl: './app.css' // defines the location of css
 })
@@ -17,6 +18,7 @@ export class App {
 
   showRegisterModal = false;
   showLoginModal = false;
+  showRegisterSuccess = false;
 
   constructor(public authService: AuthService) {}
 
@@ -26,6 +28,16 @@ export class App {
 
   closeRegister() {
     this.showRegisterModal = false;
+  }
+
+  handleRegistrationSuccess() {
+    this.showRegisterModal = false;
+    this.showRegisterSuccess = true;
+  }
+
+  goToLoginFromSuccess() {
+    this.showRegisterSuccess = false;
+    this.showLoginModal = true;
   }
 
   openLogin() {
