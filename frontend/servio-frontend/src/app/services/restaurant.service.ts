bb444704b104b9304905213e5fafa09a64c6647a
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-28 18:44:00 +0100

    Implemented restaurant service for fetching restaurants from backend

---
+++ frontend/servio-frontend/src/app/services/restaurant.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Restaurant { // matches the structure of the restaurant obj from backend
+  id: number;
+  name: string;
+  cuisine: string;
+  delivery_fee: number;
+  minimum_order_value: number;
+}
+
+@Injectable({
+  providedIn: 'root' // one instance of the class
+})
+export class RestaurantService {
+  private apiUrl = 'http://localhost:3000/restaurants';
+
+  constructor(private http: HttpClient) {}
+
+  getRestaurants() {
+    return this.http.get<Restaurant[]>(this.apiUrl); // returns array of restaurant objects
+  }
+}
