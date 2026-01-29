adf3a08f8b0e1659e84569183a048b8fc0240f1c
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-29 21:28:52 +0100

    Added horizontal scroll logic and arrow state handling cuisine categories

--- frontend/servio-frontend/src/app/components/cuisine-categories/cuisine-categories.ts
+++ frontend/servio-frontend/src/app/components/cuisine-categories/cuisine-categories.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { CUISINES, Cuisine } from '../../data/cuisines';
 
 @Component({
@@ -7,13 +7,43 @@ import { CUISINES, Cuisine } from '../../data/cuisines';
   templateUrl: './cuisine-categories.html',
   styleUrl: './cuisine-categories.css'
 })
-export class CuisineCategoriesComponent {
+export class CuisineCategoriesComponent implements AfterViewInit {
   cuisines: Cuisine[] = CUISINES;
 
   @Output() cuisineSelected = new EventEmitter<string>();
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef<HTMLDivElement>;
+
+  canScrollLeft = false;
+  canScrollRight = false;
 
   // called when the cuisine banner is clicked
   selectCuisine(cuisineKey: string) {
     this.cuisineSelected.emit(cuisineKey); // sends the value out of the component ('japanese')
   }
+
+  // gets activated on first page loading
+  ngAfterViewInit() {
+    this.updateScrollArrows();
+  }
+
+  // gets activated when user scrolls
+  onScroll() {
+    this.updateScrollArrows();
+  }
+
+  updateScrollArrows() {
+    const el = this.scrollContainer.nativeElement;
+
+    this.canScrollLeft = el.scrollLeft > 0; // how far the content is scrolled from the left edge
+    this.canScrollRight = el.scrollLeft + el.clientWidth < el.scrollWidth;
+  }
+
+  scrollLeft() {
+    this.scrollContainer.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+
+  scrollRight() {
+    this.scrollContainer.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
+
 }
