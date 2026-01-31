c7d26acfe79cb88c504d9c6f79b2e531d6d7b01f
Klavdija <klavdija.markovic.dev@gmail.com>
2026-01-31 21:51:24 +0100

    Wired Restaurant-list component into app.html

--- frontend/servio-frontend/src/app/app.ts
+++ frontend/servio-frontend/src/app/app.ts
@@ -5,11 +5,13 @@ import { RegistrationSuccessModal } from './components/registration-success-moda
 import {LoginModal} from './components/login-modal/login-modal';
 import {CuisineCategoriesComponent} from './components/cuisine-categories/cuisine-categories';
 import { RestaurantService, Restaurant } from './services/restaurant.service';
+import { RestaurantsListComponent } from './components/restaurant-list/restaurant-list';
+
 
 // root UI component definition
 @Component({
   selector: 'app-root', // this component will be rendered when app-root appears in html
-  imports: [RegisterModal, LoginModal, RegistrationSuccessModal, CuisineCategoriesComponent ], // what the component is allowed to use
+  imports: [RegisterModal, LoginModal, RegistrationSuccessModal, CuisineCategoriesComponent, RestaurantsListComponent ], // what the component is allowed to use
   templateUrl: './app.html', // defines the location of the html for this component
   styleUrl: './app.css' // defines the location of css
 })
