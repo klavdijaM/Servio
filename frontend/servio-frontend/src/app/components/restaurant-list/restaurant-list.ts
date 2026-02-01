e2139b74b52662a8d11bf7b4c8376c51d4028e06
Klavdija <klavdija.markovic.dev@gmail.com>
2026-02-01 19:09:12 +0100

    Added router navigation to restaurant cards for single restaurant pages

--- frontend/servio-frontend/src/app/components/restaurant-list/restaurant-list.ts
+++ frontend/servio-frontend/src/app/components/restaurant-list/restaurant-list.ts
@@ -1,19 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { Restaurant } from '../../services/restaurant.service';
+import { Router } from '@angular/router';
 import { RESTAURANTS } from '../../data/restaurants';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-restaurants-list',
   standalone: true,
-  templateUrl: './restaurants-list.html',
-  styleUrl: './restaurants-list.css'
+  imports: [CommonModule],
+  templateUrl: './restaurant-list.html',
+  styleUrl: './restaurant-list.css'
 })
+
 export class RestaurantsListComponent {
   // allows the variable to be set from outside - the component does not own restaurant data, it has to receive it
   @Input() restaurants: Restaurant[] = [];
+
+  constructor(private router: Router) {}
 
   getRestaurantImage(name: string): string {
     const match = RESTAURANTS.find(r => r.name === name);
     return match ? match.image : 'assets/restaurants/placeholder.webp';
   }
+
+  openRestaurant(id: number) {
+    this.router.navigate(['/restaurants', id]); // changes url to /restaurants/id to get a single restaurant page
+  }
+
 }
