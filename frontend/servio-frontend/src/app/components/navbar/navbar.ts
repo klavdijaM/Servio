db9f86a5cd1f9104d3aa39bf1ce81f43b263a8ec
Klavdija <klavdija.markovic.dev@gmail.com>
2026-02-03 19:00:19 +0100

    Implemented navigation from navbar to the profile page

--- frontend/servio-frontend/src/app/components/navbar/navbar.ts
+++ frontend/servio-frontend/src/app/components/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -10,5 +11,13 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './navbar.css'
 })
 export class NavbarComponent {
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService, // public to make it accessible in html
+    private router: Router,) {}
+
+  goToProfile() {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/profile']);
+    }
+  }
 }
