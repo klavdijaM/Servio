b551ae9513eab2aa6067783218b3beb1593e7533
Klavdija <klavdija.markovic.dev@gmail.com>
2026-02-03 18:47:40 +0100

    Added password change form with frontend validation and errors

--- frontend/servio-frontend/src/app/components/profile/profile.ts
+++ frontend/servio-frontend/src/app/components/profile/profile.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-profile',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
@@ -15,7 +16,7 @@ export class ProfileComponent {
   currentPassword = '';
   newPassword = '';
 
-  errorMessage = '';
+  currentPasswordError = '';
   successMessage = '';
 
   constructor(
@@ -24,7 +25,7 @@ export class ProfileComponent {
   ) {}
 
   changePassword() {
-    this.errorMessage = '';
+    this.currentPasswordError = '';
     this.successMessage = '';
 
     this.http.put(
@@ -46,10 +47,11 @@ export class ProfileComponent {
         this.newPassword = '';
       },
       error: (err) => {
+        this.currentPasswordError = '';
         if (err.status === 401) {
-          this.errorMessage = 'Current password is not correct';
+          this.currentPasswordError = 'Current password is not correct';
         } else {
-          this.errorMessage = 'Failed to update password';
+          this.currentPasswordError = 'Failed to update password';
         }
       }
     });
