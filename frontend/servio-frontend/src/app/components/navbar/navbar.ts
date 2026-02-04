36c09c2eb0fc6a494b0d025343644f3c99adf111
Klavdija <klavdija.markovic.dev@gmail.com>
2026-02-04 19:12:01 +0100

    Wired the dropdown cart component to the navbar layout with the toggle button to open and close the cart popup

--- frontend/servio-frontend/src/app/components/navbar/navbar.ts
+++ frontend/servio-frontend/src/app/components/navbar/navbar.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import {Router} from '@angular/router';
 import {CartService} from '../../services/cart.service';
+import {CartDropdownComponent} from '../cart-dropdown/cart-dropdown';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CartDropdownComponent],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class NavbarComponent {
+  isCartOpen = false;
+
   constructor(
     public authService: AuthService, // public to make it accessible in html
     private router: Router,
@@ -29,6 +32,10 @@ export class NavbarComponent {
 
   get cartCount(): number {
     return this.cartService.getItemCount();
+  }
+
+  toggleCart() {
+    this.isCartOpen = !this.isCartOpen;
   }
 
 }
